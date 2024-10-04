914e69d7213455122542c1ec6b404f25f95d92a9
Moises <moisesbastos8@gmail.com>
2024-10-04 20:54:56 -0300

    Pomodoro Cáotica

---
+++ app/src/main/java/com/example/pomocaotica/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.pomocaotica
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.pomocaotica.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root )
+
+        binding.bntSobre.setOnClickListener{
+            val intent = Intent(this, AboutActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
